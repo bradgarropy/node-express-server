02eee0cb192cf8a1d64e97dab45af64d3d183b87
Brad Garropy <bradgarropy@gmail.com>
2017-07-06 00:05:44 -0500

    Basic routing.

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const app = express();
+
+
+app.get("/", function(req, res) {
+    res.send(req.method + req.originalUrl);
+});
+
+app.post("/", function(req, res) {
+    res.send(req.method + req.originalUrl);
+});
+
+app.patch("/", function(req, res) {
+    res.send(req.method + req.originalUrl);
+});
+
+app.delete("/", function(req, res) {
+    res.send(req.method + req.originalUrl);
+});
 
 
 // start application
 const port = 3000;
-const app = express();
 app.listen(port, function() {
     console.log("Server listening on port %s.", port);
 });
